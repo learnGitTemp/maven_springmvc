7e85022b22903da17a01f92a3866356c4b79dbb5
tangtaiming <accountnum@sina.cn>
2016-07-26 22:58:42 +0800

    提交post请求

--- src/main/java/com/orm/AnQuan.java
+++ src/main/java/com/orm/AnQuan.java
@@ -1,14 +1,19 @@
 package com.orm;
 
+import java.util.List;
+
 /**
  * Created by Administrator on 2016/4/6.
  */
 public class AnQuan {
 
-
     private Integer id;
 
     private String name;
+
+    private List<String> email;
+
+    private List<Integer> categoryId;
 
     public Integer getId() {
         return id;
@@ -26,4 +31,19 @@ public class AnQuan {
         this.name = name;
     }
 
+    public List<String> getEmail() {
+        return email;
+    }
+
+    public void setEmail(List<String> email) {
+        this.email = email;
+    }
+
+    public List<Integer> getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(List<Integer> categoryId) {
+        this.categoryId = categoryId;
+    }
 }
